bc79480a9f081664d4b87a63e59b1047f62c28ea
Arturo Pie <arturop@nulogy.com>
2019-07-19 15:53:40 -0400

    parse event

--- integrate/entrypoint.rb
+++ integrate/entrypoint.rb
@@ -1,5 +1,8 @@
+require "json"
+
 puts "GITHUB_SHA: #{ENV["GITHUB_SHA"]}"
 puts "GITHUB_REF: #{ENV["GITHUB_REF"]}"
 puts "GITHUB_EVENT_PATH: #{ENV["GITHUB_EVENT_PATH"]}"
-file = File.read(ENV["GITHUB_EVENT_PATH"])
-puts file
+puts File.read(ENV["GITHUB_EVENT_PATH"])
+
+event = JSON.parse(File.read(ENV["GITHUB_EVENT_PATH"]))
